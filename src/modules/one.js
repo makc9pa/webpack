a694fb779f4c08d5a4387f8ed8558364eb55aac6
makc9pa <2max2@mail.ru>
2022-01-14 19:44:27 +0300

    feat(lesson18): создал модульную структуру проекта с помощью автосборки через webpack

---
+++ src/modules/one.js
@@ -0,0 +1,6 @@
+const oneModule = () => {
+    const one = 'Module One'
+    console.log(one)
+}
+
+export default oneModule
---
+++ src/modules/two.js
@@ -0,0 +1,6 @@
+const twoModule = () => {
+    const two = 'Module Two'
+    console.log(two)
+}
+
+export default twoModule
